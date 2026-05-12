d1f44ad51f7fdc1d533da2b137c15385e0905cb9
DESKTOP-FNEFHL3\Daniil <66742@gymnasium.ax>
2026-05-12 13:42:57 +0300

    Klar med sammanställning hämta

--- api/src/Compilations.php
+++ api/src/Compilations.php
@@ -9,7 +9,7 @@ declare (strict_types=1);
  * @return Response
  */
 function compilations(Route $route): Response {
-    return new Response("Compilations");
+
     try {
         if (count($route->getParams()) === 2 && $route->getMethod() === RequestMethod::GET) {
             return hamtaSammanstallning($route->getParams()[0], $route->getParams()[1]);
@@ -28,4 +28,62 @@ function compilations(Route $route): Response {
  * @return Response
  */
 function hamtaSammanstallning(string $from, string $tom): Response {
+
+    // Kontrollera indata
+    $fromDate=DateTimeImmutable::createFromFormat('Y-m-d', $from);
+    $tomDate=DateTimeImmutable::createFromFormat('Y-m-d', $tom);
+
+    $err=[];
+    if($fromDate===false) {
+        $err[]="Ogiltigt från-datum";
+    } elseif($fromDate->format('Y-m-d')!==$from) {
+        $err[]="Ogiltigt format på från-datum";
+    }
+
+    if($tomDate===false) {
+        $err[]="Ogiltigt till-datum";
+    }  elseif($tomDate->format('Y-m-d')!==$tom) {
+        $err[] = "Ogiltigt format på till-datum";
+    }
+    if(count($err)===0 && $fromDate->format('Y-m-d')>$tomDate->format('Y-m-d')) {
+        $err[]="Från-datum ska vara mindre än till-datum";
+    }
+
+    if(count($err)>0) {
+        array_unshift($err, "Bad request");
+        $retur=new stdClass();
+        $retur->error=$err;
+        return new Response($retur, 400);
+    }
+
+    // Koppla databas
+    $db=connectDb();
+
+
+
+
+    // Skicka fråga
+    $stmt=$db->prepare('SELECT aktivitet_id, aktivitet,
+SEC_TO_TIME(SUM(TIME_TO_SEC(varaktighet))) As time
+FROM uppgifter
+INNER JOIN aktiviteter ON aktiviteter.id=aktivitet_id
+WHERE datum BETWEEN :from AND :tom
+GROUP BY aktivitet_id, aktivitet
+ORDER BY aktivitet');
+    $stmt->execute(['from'=>$fromDate->format('Y-m-d'),'tom'=>$tomDate->format('Y-m-d')]);
+
+    // Returnera svar
+    $rader=[];
+    foreach ($stmt->fetchAll() as $row) {
+        $post=new stdClass();
+        $post->aktivitet_id=$row['aktivitet_id'];
+        $post->aktivitet=$row['aktivitet'];
+        $post->time= substr ($row['time'], 0, -3);
+        $rader[] = $post;
+    }
+
+    $retur=new stdClass();
+    $retur->tasks=$rader;
+
+    return new Response($retur);
 }
